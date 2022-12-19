14b1e92e787cfe88e50d1579dc2dbc1c89b1fc00
ezequiasmelo <ezequias-rm@hotmail.com>
2022-12-19 11:42:14 -0300

    update home page

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -10,55 +10,57 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(140),
+        preferredSize:
+            Size.fromHeight(120), // preferredSize: Size.fromHeight(140),
         child: HomeAppBar(),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: CustomScrollView(
-                scrollDirection: Axis.vertical,
-                slivers: <Widget>[
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(vertical: 24.0),
-                    sliver: SliverFixedExtentList(
-                      itemExtent: 300.0,
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                          return GestureDetector(
-                            onTap: () => {
-                              // Navigator.pushReplacementNamed(
-                              //   context,
-                              //   NamedRoute.details,
-                              //   arguments: polls[index],
-                              // )
-                            },
-                            child: CustomBigCard(courses[index]),
-                          );
-                        },
-                        childCount: courses.length,
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: CustomScrollView(
+                  scrollDirection: Axis.vertical,
+                  slivers: <Widget>[
+                    SliverPadding(
+                      padding: const EdgeInsets.symmetric(vertical: 24.0),
+                      sliver: SliverFixedExtentList(
+                        itemExtent: 300.0,
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return GestureDetector(
+                              onTap: () => {
+                                // Navigator.pushReplacementNamed(
+                                //   context,
+                                //   NamedRoute.details,
+                                //   arguments: polls[index],
+                                // )
+                              },
+                              child: CustomBigCard(courses[index]),
+                            );
+                          },
+                          childCount: courses.length,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                //     ListView.separated(
+                //   physics: NeverScrollableScrollPhysics(),
+                //   shrinkWrap: true,
+                //   itemCount: courses.length,
+                //   itemBuilder: (_, idx) => CustomCard(
+                //     child: Text('ew'),
+                //   ),
+                //   separatorBuilder: (_, __) => const SizedBox(height: 10),
+                // ),
               ),
-              //     ListView.separated(
-              //   physics: NeverScrollableScrollPhysics(),
-              //   shrinkWrap: true,
-              //   itemCount: courses.length,
-              //   itemBuilder: (_, idx) => CustomCard(
-              //     child: Text('ew'),
-              //   ),
-              //   separatorBuilder: (_, __) => const SizedBox(height: 10),
-              // ),
-            ),
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
